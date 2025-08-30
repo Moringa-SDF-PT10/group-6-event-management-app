42e4cc3d28aaa4ad3c0e4263741e94dbc3cf89a8
Thiggz <Mwithigaike@gmail.com>
2025-08-30 17:48:18 +0100

    Update render-build.sh

--- server/render-build.sh
+++ server/render-build.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
-# exit on error
+
 set -o errexit
 
 echo "Building the project..."
 
-# Install backend dependencies
 pip install -r requirements.txt
 
-# Install frontend dependencies and build the static files
+
 npm install --prefix ../client
 npm run build --prefix ../client
 
-# Run database migrations
+
+export FLASK_APP="app:create_app()"
+
+
 echo "Running database migrations..."
 flask db upgrade
 
-# Run the seed script to populate the database with initial data
 echo "Seeding the database..."
 python app/seed.py
 
